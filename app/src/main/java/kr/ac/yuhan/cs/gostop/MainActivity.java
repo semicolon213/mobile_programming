8ad55dd703b3a7237cf78f6ec8d00e424457d836
semicolon213 <khk213624@gmail.com>
2025-05-05 22:03:53 +0900

    PWA 설정 코드 추가

--- app/src/main/java/kr/ac/yuhan/cs/gostop/MainActivity.java
+++ app/src/main/java/kr/ac/yuhan/cs/gostop/MainActivity.java
@@ -1,6 +1,9 @@
 package kr.ac.yuhan.cs.gostop;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        webView = findViewById(R.id.webview);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);             ///JS 활성화
+        webSettings.setDomStorageEnabled(true);             ///PWA 사용시 필요
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setSupportZoom(false);
+
+        webView.setWebViewClient(new WebViewClient());      ///외부 브라우저 안 열리도록 설정
+
+        // React PWA 사이트 주소 입력
+        webView.loadUrl("http://localhost:8080");
+    }
+    @Override
+    public void onBackPressed() {                           ///화면에서 뒤로가기 사용시 PWA 사이트 뒤로가기
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
