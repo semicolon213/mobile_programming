721e61dd8e864deaec51ef4cc194ef035e4abb96
unknown <ghksgml0807@naver.com>
2025-06-09 09:32:26 +0900

    Amulater Tmap show avaliable

--- app/src/main/java/kr/ac/yuhan/cs/gostop/MainActivity.java
+++ app/src/main/java/kr/ac/yuhan/cs/gostop/MainActivity.java
@@ -1,20 +1,28 @@
 package kr.ac.yuhan.cs.gostop;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
     private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,19 +37,23 @@ public class MainActivity extends AppCompatActivity {
         webView = findViewById(R.id.webview);
 
         WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);             ///JS 활성화
-        webSettings.setDomStorageEnabled(true);             ///PWA 사용시 필요
+        webSettings.setJavaScriptEnabled(true);             /// JS 활성화
+        webSettings.setDomStorageEnabled(true);             /// PWA 사용 시 필요
         webSettings.setLoadWithOverviewMode(true);
         webSettings.setUseWideViewPort(true);
         webSettings.setSupportZoom(false);
 
-        webView.setWebViewClient(new WebViewClient());      ///외부 브라우저 안 열리도록 설정
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+
+        webView.setWebViewClient(new WebViewClient());      /// 외부 브라우저 안 열리도록 설정
 
         // React PWA 사이트 주소 입력
-        webView.loadUrl("yuhangostop.netlify.app");
+        webView.loadUrl("https://yuhangostop.netlify.app/");
     }
     @Override
-    public void onBackPressed() {                           ///화면에서 뒤로가기 사용시 PWA 사이트 뒤로가기
+    public void onBackPressed() {                           /// 화면에서 뒤로가기 사용 시 PWA 사이트 뒤로가기
         if (webView.canGoBack()) {
             webView.goBack();
         } else {
